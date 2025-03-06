d45cfda014fbcec461c24ef5bcbd2165d207a54f
jjy3385 <jjy3386@gmail.com>
2025-03-06 23:03:01 +0900

    diagram 이미지 추가

--- src/main/java/knou/seoul/hanwoori/domain/member/MemberController.java
+++ src/main/java/knou/seoul/hanwoori/domain/member/MemberController.java
@@ -1,9 +1,7 @@
 package knou.seoul.hanwoori.domain.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
